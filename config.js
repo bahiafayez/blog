1049aa261b12721e19754fffde42bcc2851d3a7d
Bahia El-Sharkawy <bahia.sharkawy@gmail.com>
2015-06-26 15:43:49 -0700

    Update url to point to alias

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ if (process.env.OPENSHIFT_MYSQL_DB_HOST != undefined) {
         // When running Ghost in the wild, use the production environment
         // Configure your URL and mail settings here
         production: {
-            url: 'http://'+process.env.OPENSHIFT_APP_DNS,
+            url: 'http://'+process.env.OPENSHIFT_APP_DNS_ALIAS,
             mail: {
                 transport: 'SMTP',
                     options: {
@@ -51,7 +51,7 @@ if (process.env.OPENSHIFT_MYSQL_DB_HOST != undefined) {
         // When running Ghost in the wild, use the production environment
         // Configure your URL and mail settings here
         production: {
-            url: 'http://'+process.env.OPENSHIFT_APP_DNS,
+            url: 'http://'+process.env.OPENSHIFT_APP_DNS_ALIAS,
             mail: {
                 transport: 'SMTP',
                     options: {
@@ -129,7 +129,7 @@ if (process.env.OPENSHIFT_MYSQL_DB_HOST != undefined) {
         // When running Ghost in the wild, use the production environment
         // Configure your URL and mail settings here
         production: {
-            url: 'http://'+process.env.OPENSHIFT_APP_DNS,
+            url: 'http://'+process.env.OPENSHIFT_APP_DNS_ALIAS,
             mail: {
                 transport: 'SMTP',
                     options: {
